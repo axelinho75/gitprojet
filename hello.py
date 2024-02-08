4bd21f04740351499a8b0cf5b8c26ee9adb3218c
Axel Flahaut <axel.flahaut@ecoles-epsi.net>
2024-02-08 12:51:17 +0100

    Age

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 a= input("quelle est votre nom? ")
 b= input("quelle est votre prenom? ")
+c= input("quelle est votre age? ")
 
 print("Bonjour, " + a + " "+ b +"!")
-
-c= input("quelle est votre age? ")
 print("vous avez " + c + " ans")
-
 
